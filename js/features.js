c356eb0770ea2a1863759c6fe180a76c5c2091d2
hadi heidariazar <fakeemail@gmail.com>
2023-06-14 23:30:43 +0330

    finished first page

--- js/features.js
+++ js/features.js
@@ -1,7 +1,29 @@
 const $ = document
+const menuElem = $.querySelector('.menu')
+const iconMenuElem = $.querySelector('.icon-menu')
+const menuItemsElem = $.querySelector('.menu-items')
 
-const loaderPage = $.querySelector('.loader')
+const loaderPageElem = $.querySelector('.loader')
 
 window.addEventListener('load', () => {
-    loaderPage.style.display = 'none'
+    loaderPageElem.classList.add('hidden')
+})
+
+iconMenuElem.addEventListener('click', () => {
+    if (menuElem.className === 'menu bg-blue p-2 py-1 show') {
+
+        menuElem.classList.remove('show')
+        menuElem.classList.add('not-show')
+        menuItemsElem.classList.add('none')
+        
+    } else {
+
+        menuElem.classList.remove('not-show')
+        menuElem.classList.add('show')
+
+        setTimeout(() => {
+            menuItemsElem.classList.remove('none')
+        }, 500)
+
+    }
 })
